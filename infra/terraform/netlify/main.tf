b2b3de9f54f7c181872fb63e44460c768756fd8c
keitaroooo <keitaro.cryptocurrency@gmail.com>
2026-04-18 14:39:57 +0900

    chore: NODE_VERSION 環境変数を削除（.node-version で管理）

--- infra/terraform/netlify/main.tf
+++ infra/terraform/netlify/main.tf
@@ -14,8 +14,6 @@ import {
   id = "c608a141-5fa6-4f1c-8884-8688484c9895"
 }
 
-# NODE_VERSION は新規作成のため import 不要（Netlify 上に未設定だった）
-
 # ビルド設定
 resource "netlify_site_build_settings" "blog" {
   site_id           = data.netlify_site.blog.id
@@ -25,14 +23,4 @@ resource "netlify_site_build_settings" "blog" {
   base_directory    = "node/app"
 }
 
-# Node.js バージョンを環境変数で指定（Dependency management の UI 設定を上書き）
-resource "netlify_environment_variable" "node_version" {
-  site_id = data.netlify_site.blog.id
-  key     = "NODE_VERSION"
-  values = [
-    {
-      value   = "22"
-      context = "all"
-    }
-  ]
-}
+# Node.js バージョンは node/app/.node-version で管理（Netlify が自動検出）
